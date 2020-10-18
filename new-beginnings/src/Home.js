3d1a1ea6c4d461f88e9ae57f0132888acd9708a7
Jacqueline Montes-De Leon <jmontesd@ucsc.edu>
2020-10-18 06:40:35 -0700

    Adding Homepage

--- new-beginnings/src/Home.js
+++ new-beginnings/src/Home.js
@@ -2,17 +2,11 @@ import React from 'react';
 import { Parallax } from 'react-parallax';
 import './Home.css';
 
-const image1="https://i.pinimg.com/originals/de/f7/21/def7210d0eb8f98ca7c7459f952c4018.jpg"
-const image2="https://www.itl.cat/pngfile/big/32-322680_plant-aesthetic-desktop-wallpaper-hd-plant.jpg"
+const image1="https://i.imgur.com/xHGk91K.png"
+const image2="https://i.imgur.com/Mu8NwCq.png"
+const image3="https://i.imgur.com/NFALSQx.png"
+const image4="https://i.imgur.com/jT8xF5q.png"
 
-const inlineStyle = {
-  background: '#fff',
-  left: '50%',
-  top: '50%',
-  position: 'absolute',
-  padding: '20px',
-  transform: 'translate(-50%, -50%)'
-}
 function Home() {
   return (
     // <div className='hero-container'>
@@ -21,24 +15,24 @@ function Home() {
     // </div>
 
     <div className="Home">
-      <Parallax bgImage={image1} strength={750}>
+      <Parallax bgImage={image1} strength={130}>
         <div style={{height:800}}>
-          <div style={inlineStyle}>
-            <h1> New Beginnings </h1>
-            <p>Believe In Yourself and What You Are Capable Of</p>
-          </div>
         </div>
       </Parallax>
       <h1>  </h1>
-      <Parallax bgImage={image2} strength={750}>
+      <Parallax bgImage={image2} strength={130}>
         <div style={{height:800}}>
-          <div style={inlineStyle}>New Beginnings</div>
         </div>
       </Parallax>
-
-
-
-      <div style={{height:'100vh'}}></div>
+      <Parallax bgImage={image3} strength={200}>
+        <div style={{height:800}}>
+        </div>
+      </Parallax>
+      <Parallax bgImage={image4} strength={200}>
+        <div style={{height:800}}>
+        </div>
+      </Parallax>
+      
     </div>
 
   );
