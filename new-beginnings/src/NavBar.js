90ef35f3d7e8d817c0d8b92c2b4bffaefa4e5ac0
Jasleen Kaur <99jasleenkaur@gmail.com>
2020-10-18 05:02:48 -0700

    nav changes

--- new-beginnings/src/NavBar.js
+++ new-beginnings/src/NavBar.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import { Link } from '@reach/router';
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+import logo from './images/newbeg3.png';
+import './NavBar.css'
 
 class NavBar extends Component {
   render() {
     return (
         <div>
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand href="#home"> LOGO </Navbar.Brand>
+    <Navbar className="color-nav" variant="light">
+      <Navbar.Brand src={logo} href="/"> <img
+        src={logo}
+        width="120"
+        height="70"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      /> </Navbar.Brand>
       <Nav className="mr-auto">
         <Link to='/' className="nav-link">Home</Link>
         <Link to='/TeamForm' className="nav-link">Find your Teammates!</Link>
