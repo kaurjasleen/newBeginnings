1d03fb922ded2f2af90e4ba22eb8516a2deaf6ea
Jasleen Kaur <99jasleenkaur@gmail.com>
2020-10-17 17:49:07 -0700

    push

---
+++ new-beginnings/src/NavBar.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Link } from '@reach/router';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+
+class NavBar extends Component {
+  render() {
+    return (
+        <div>
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="#home"> LOGO </Navbar.Brand>
+      <Nav className="mr-auto">
+        <Link to='/' className="nav-link">Home</Link>
+        <Link to='/TeamForm' className="nav-link">Find your Teammates!</Link>
+        <Link to='/HowToGetStarted' className="nav-link">How To Get Started</Link>
+        <Link to='/PrevSubmissions' className="nav-link">Previous Submissions</Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-primary">Search</Button>
+      </Form>
+    </Navbar>
+  </div>
+    );
+  }
+}
+export default NavBar;
